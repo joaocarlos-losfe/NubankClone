f3e84089dcddc2a1ffb74631e92a98dc655e6970
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2021-02-24 10:53:11 -0300

    Adicionar arquivos de projeto.

---
+++ NubankClone/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NubankClone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void cartClick_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
